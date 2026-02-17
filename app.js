18f573564a88fd52e1806bbb0d945276a5951f62
Juan David <jdgranadillo9@gmail.com>
2026-02-16 20:59:15 -0400

    borrado el contenido viejo comentado

--- app.js
+++ app.js
@@ -1,38 +1,4 @@
-/* const carrito = [];
 
-const agregarAlCarrito = (producto) => {
-    carrito.push(producto);
-}
-
-const quitarDelCarrito = () => {
-    carrito.pop();
-}
-
-const verCarrito = () => {
-    let listaCarrito = carrito.join(", ");
-    return listaCarrito;
-}
-
-let menu = prompt('Que quieres hacer? \n1- Agregar producto al carrito \n2- Quitar ultimo producto del carrito \n3- Ver carrito \n4- Salir');
-
-while (menu !== '4') {
-    switch (menu) {
-        case '1':
-            let producto = prompt("Ingrese el nombre del producto a agregar al carrito:");
-            agregarAlCarrito(producto);
-            break;
-        case '2':
-            quitarDelCarrito();
-            alert("Ultimo producto eliminado del carrito.");
-            break;
-        case '3':
-            alert("Productos en el carrito: " + verCarrito());
-            break;
-        default:
-            alert("Opcion no valida, por favor intente de nuevo.");
-    }
-    menu = prompt('Que quieres hacer? \n1- Agregar producto al carrito \n2- Quitar ultimo producto del carrito \n3- Ver carrito \n4- Salir');
-} */
 
 let carrito = [];
 let carritoGuardado = localStorage.getItem('carrito');
