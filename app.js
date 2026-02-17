5ff650871fda6238de88c6a30d5c98a256dd6c96
Juan David <jdgranadillo9@gmail.com>
2026-02-16 20:56:56 -0400

    Agregado contenido clases 5-7

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const carrito = [];
+/* const carrito = [];
 
 const agregarAlCarrito = (producto) => {
     carrito.push(producto);
@@ -32,4 +32,67 @@ while (menu !== '4') {
             alert("Opcion no valida, por favor intente de nuevo.");
     }
     menu = prompt('Que quieres hacer? \n1- Agregar producto al carrito \n2- Quitar ultimo producto del carrito \n3- Ver carrito \n4- Salir');
+} */
+
+let carrito = [];
+let carritoGuardado = localStorage.getItem('carrito');
+
+if (carritoGuardado !== null) {
+    carrito = JSON.parse(carritoGuardado);
 }
+
+class Producto {
+    constructor(nombre, cantidad) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+    }
+}
+
+const inputProducto = document.getElementById('productoInput');
+const inputCantidad = document.getElementById('cantidadInput');
+const btnAgregar = document.getElementById('btnAgregar');
+const btnQuitar = document.getElementById('btnQuitar');
+const contenedorCarrito = document.getElementById('listaCarrito');
+
+const actualizarCarrito = () => {
+    let contenidoHTML = ``;
+
+    carrito.forEach(producto => {
+        contenidoHTML += `<li>${producto.nombre} - Cantidad: ${producto.cantidad}</li>`;
+    });
+
+    contenedorCarrito.innerHTML = contenidoHTML;
+
+    let carritoString = JSON.stringify(carrito);
+    localStorage.setItem('carrito', carritoString);
+}
+
+btnAgregar.addEventListener('click', () => {
+    let nombreProducto = inputProducto.value.trim();
+
+    if (nombreProducto !== '') {
+        const cantidadProducto = parseInt(inputCantidad.value.trim());
+        if (!cantidadProducto || cantidadProducto <= 0) {
+            alert('Por favor, ingresa una cantidad válida.');
+            return;
+        }
+        const nuevoProducto = new Producto(nombreProducto, cantidadProducto);
+        carrito.push(nuevoProducto);
+        inputProducto.value = '';
+        inputCantidad.value = '';
+        actualizarCarrito();
+    } else {
+        alert('Por favor, ingresa el nombre de un producto.');
+    }
+});
+
+btnQuitar.addEventListener('click', () => {
+    if (carrito.length > 0) {
+        carrito.pop();
+        actualizarCarrito();
+    } else {
+        alert('El carrito ya está vacío.');
+    }
+});
+
+actualizarCarrito();
